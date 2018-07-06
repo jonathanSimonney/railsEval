010e3bd2ec7bab882dce184040460927a18fa0cc
SUP'Internet 15 <supinternet15@MacBook-Air-de-SUPInternet-8.local>
2018-07-06 15:21:28 +0200

    added the pages routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/super-page' => 'pages#show',  id: 'super-page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#index'
 
@@ -7,4 +8,9 @@ Rails.application.routes.draw do
   resources :gazooies, only: [:index, :show, :create]
   resources :hash_tags, only: [:show]
   resources :news
+  resources :pages, except: [:index]
+
+
+
+  get '/:id' => 'pages#show'
 end
